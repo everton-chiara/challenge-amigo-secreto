70955483df558346d7bd23963704a283e81fb516
Everton Chiara <everton-csilva@outlook.com>
2025-02-18 16:14:04 -0300

    Tornando a pagina funcional

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let amigos = [];
+
+function adicionarAmigo() {
+    const inputNome = document.getElementById('amigo');
+    const nome = inputNome.value.trim();
+
+    if (nome === "") {
+        alert("Por favor, insira um nome válido.");
+        return;
+    }
+
+    amigos.push(nome);
+
+    inputNome.value = "";
+
+    atualizarLista();
+}
+
+function atualizarLista() {
+    const listaAmigos = document.getElementById("listaAmigos");
+    listaAmigos.innerHTML = "";
+
+    amigos.forEach(function(amigo) {
+        const li = document.createElement("li");
+        li.textContent = amigo;
+        listaAmigos.appendChild(li);
+    });
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("Adicione pelo menos um amigo para realizar o sorteio.");
+        return;
+    }
+
+    const indiceSorteado = Math.floor(Math.random() * amigos.length);
+    const amigoSorteado = amigos[indiceSorteado];
+
+    const resultado = document.getElementById("resultado");
+    resultado.innerHTML = `<li>O amigo secreto é: ${amigoSorteado}</li>`;
+}
